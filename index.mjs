659d6e27e5b72bc0ba8c0115bc1cc3cf6d5cb8c5
Zonaairdrop21 <155278715+Zonaairdrop21@users.noreply.github.com>
2025-07-13 07:50:36 +0700

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -86,10 +86,10 @@ const display_welcome_screen = async () => {
 };
 
 
-const RPC_URL = 'https://testnet-rpc.zigchain.com';
+const RPC_URL = 'https://rpc.zigscan.net/';
 const API_URL = 'https://testnet-api.oroswap.org/api/'; // Ensure trailing slash
 const EXPLORER_URL = 'https://zigscan.org/tx/';
-const GAS_PRICE = GasPrice.fromString('0.026uzig');
+const GAS_PRICE = GasPrice.fromString('0.027uzig');
 
 // Only token aktif: ZIG, ORO, NFA, CULTCOIN
 const TOKEN_SYMBOLS = {
