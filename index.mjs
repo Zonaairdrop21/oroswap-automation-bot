92be9f1383c00a96526291744c63a03346c90670
Zonaairdrop21 <155278715+Zonaairdrop21@users.noreply.github.com>
2025-07-18 23:21:23 +0700

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -302,7 +302,7 @@ async function performSwap(wallet, address, amount, pairName, swapNumber, fromDe
     const microAmount = toMicroUnits(amount, fromDenom);
     const poolInfo = await getPoolInfo(pair.contract, rpcClient);
     const beliefPrice = calculateBeliefPrice(poolInfo, pairName, fromDenom);
-    const maxSpread = "0.5";
+    const maxSpread = "0.3";
     const msg = {
       swap: {
         belief_price: beliefPrice,
